9295b7e477add0c57f0fedb7fda41c60adb01356
Carlos <carlosremuzzi@gmail.com>
2019-03-07 16:36:03 -0400

    piping avatar_url assignment everything in one line

--- get-avatar.sh
+++ get-avatar.sh
@@ -3,9 +3,7 @@
 USER_ID=$1
 HOMESERVER_URL=${2:-https://matrix.org}
 
-avatar_url=$(curl -s -X GET ${HOMESERVER_URL}/_matrix/client/r0/profile/${USER_ID}/avatar_url | jq -r .avatar_url)
-
-avatar_url=$(echo "${avatar_url}" | sed -e "s~mxc://~~g")
+avatar_url=$(curl -s -X GET ${HOMESERVER_URL}/_matrix/client/r0/profile/${USER_ID}/avatar_url | jq -r .avatar_url | sed -e "s~mxc://~~g")
 
 wget ${HOMESERVER_URL}/_matrix/media/r0/download/${avatar_url} -O /tmp/avatar.png
 
