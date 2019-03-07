e9f2776813aa33ec5c73faf30b5c3b0309762625
Carlos <carlosremuzzi@gmail.com>
2019-03-07 17:28:26 -0400

    adding empty result handler

--- get-avatar.sh
+++ get-avatar.sh
@@ -3,8 +3,13 @@
 USER_ID=$1
 HOMESERVER_URL=${2:-https://matrix.org}
 
-avatar_url=$(curl -s -X GET ${HOMESERVER_URL}/_matrix/client/r0/profile/${USER_ID}/avatar_url | jq -r .avatar_url | sed -e "s~mxc://~~g")
+avatar_url=$(curl -s -X GET ${HOMESERVER_URL}/_matrix/client/r0/profile/${USER_ID}/avatar_url)
 
-wget ${HOMESERVER_URL}/_matrix/media/r0/download/${avatar_url} -O /tmp/avatar.png
-
-xdg-open /tmp/avatar.png
+if [ "${avatar_url}" == "{}" ]; then
+    echo $avatar_url
+    exit 1
+else
+    short_url=$(echo "${avatar_url}"  | grep -o 'mxc.*\"' | grep -o '[^"]\+' | sed 's~mxc://~~g' )
+    wget ${HOMESERVER_URL}/_matrix/media/r0/download/${short_url} -O /tmp/avatar.png
+    xdg-open /tmp/avatar.png
+fi
