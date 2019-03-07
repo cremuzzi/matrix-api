b885a837dcba3d9b14c9a3bef352d676f8644591
Carlos <carlosremuzzi@gmail.com>
2019-03-07 15:32:13 -0400

    extracting serverName and mediaId from the avatar_url

--- get-avatar.sh
+++ get-avatar.sh
@@ -1,20 +1,23 @@
 #!/usr/bin/env sh
 
 USER_ID=$1
-SERVER_URL=$2
-SERVER_NAME=$3
+SERVER_NAME=$2
 
-avatar_url=$(curl -s -X GET ${SERVER_URL}/_matrix/client/r0/profile/@${USER_ID}:${SERVER_NAME}/avatar_url | jq -r .avatar_url)
+avatar_url=$(curl -s -X GET https://matrix.org/_matrix/client/r0/profile/${USER_ID}/avatar_url | jq -r .avatar_url)
 
 echo "avatar_url is ${avatar_url}"
 
-media_id=$(echo "${avatar_url}" | grep -oe "[^\/]*$" )
+server_name=$(echo "${avatar_url}" | sed -e "s~mxc://~~g")
+
+echo "server_name is ${server_name}"
+
+media_id=$(echo "${server_name}" | grep -oe "[^\/]*$" )
 
 echo "media_is is ${media_id}"
 
 echo "opening media..."
 
-wget -q ${SERVER_URL}/_matrix/media/r0/download/${SERVER_NAME}/${media_id} -O /tmp/avatar.png
+wget -q https://matrix.org/_matrix/media/r0/download/${server_name}/${media_id} -O /tmp/avatar.png
 
 # this could be just any image viewer maybe with xdg-open ?
 xdg-open /tmp/avatar.png
